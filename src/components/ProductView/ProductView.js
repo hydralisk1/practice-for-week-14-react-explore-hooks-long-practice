7bb079c62d86c176e48dc9336677b197ef0d9eb0
Joonil Kim <bartholomaeuskim@gmail.com>
2022-11-02 16:42:49 -0700

    finished

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
 import './ProductView.css'
@@ -6,7 +6,27 @@ import './ProductView.css'
 function ProductView({ products }) {
 
   // TODO: Replace with state variable
-  const sideOpen = true;
+  // localStorage.setItem() // getItem()
+  const opened = localStorage.getItem('sideOpen')
+  const product = localStorage.getItem('selectedProduct')
+
+  const [sideOpen, setSideOpen] = useState(opened && opened === 'true')
+  const [selectedProduct, setSelectedProduct] = useState(product ? products.find(p => p.id === Number(product)) : '')
+
+  useEffect(() => {
+    if(selectedProduct){
+      localStorage.setItem('selectedProduct', selectedProduct.id)
+      setSideOpen(true)
+    }
+  }, [selectedProduct])
+
+  useEffect(() => {
+    localStorage.setItem('sideOpen', sideOpen)
+    if(!sideOpen) {
+      localStorage.setItem('selectedProduct', null)
+      setSelectedProduct()
+    }
+  }, [sideOpen])
 
   return (
     <div className="product-view">
@@ -17,7 +37,8 @@ function ProductView({ products }) {
             <ProductListItem
               key={item.id}
               product={item}
-              onClick={() => console.log('SELECT PRODUCT', item)}
+              onClick={() => setSelectedProduct(item)}
+              isSelected={selectedProduct && item.id === selectedProduct.id}
             />
           )}
         </div>
@@ -25,11 +46,11 @@ function ProductView({ products }) {
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
           <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
+               onClick={() => setSideOpen(!sideOpen)}>
             {sideOpen ? '>' : '<'}
           </div>
         </div>
-        <ProductDetails visible={sideOpen} />
+        <ProductDetails visible={sideOpen} product={selectedProduct} />
       </div>
     </div>
   );
